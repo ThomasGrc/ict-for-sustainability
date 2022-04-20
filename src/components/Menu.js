a0cdbf2d6474f0d3aeb4bf33b49644a980bc3d70
ThomasGrc <47837271+ThomasGrc@users.noreply.github.com>
2022-04-21 01:32:25 +0200

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export function Menu() {
         <div className="menu--logo">
             Grape<span>.</span>
         </div>
-        <div className="menu--instagram">
+        <div onClick={() => window.open("https://instagram.com/")} className="menu--instagram">
             INSTAGRAM <InstagramLogo />
         </div>
     </>
